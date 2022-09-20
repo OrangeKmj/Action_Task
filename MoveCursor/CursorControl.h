8e852a11361130c9a8c3b60f408e4a925dad54ff
OrnageKmj <kmjoon98@gmail.com>
2022-09-20 17:52:08 +0900

    tetris_ver_1

--- MoveCursor/CursorControl.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <iostream>
-#include <conio.h>
-#include <vector>
-#include <Windows.h>
-#pragma comment(lib, "winmm.lib")
-using namespace std;
-
-void ControlCursorMove(int x, int y, int speed); // 커서의 움직임을 제어
--- MoveCursor/CursorInfo.h
+++
@@ -1,7 +0,0 @@
-﻿#pragma once
-#include <iostream>
-#include <sstream>
-#include <vector>
-using namespace std;
-
-vector<int>	SetCursorData(); // 입력받은 데이터에 오류가 있으면 수정하여 커서 이동 설정을 완료
