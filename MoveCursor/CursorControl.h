c04f3f5b8a52fbe52640b978a547496c74ca9c14
OrnageKmj <kmjoon98@gmail.com>
2022-09-20 17:53:07 +0900

    move cursor

---
+++ MoveCursor/CursorControl.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib")
+using namespace std;
+
+void ControlCursorMove(int x, int y, int speed); // 커서의 움직임을 제어
---
+++ MoveCursor/FrameControl.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#include <Windows.h>
+#pragma comment(lib, "winmm.lib")
+
+void SpeedControl(int cursorSpeed); // 커서가 한 번 움직이는데 걸리는 시간 제어
